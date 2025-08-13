a65f5b018fecf65ffdeb386f1796746c0c50eb5b
vid0326 <vidhutraushan9@gmail.com>
2025-08-13 10:36:46 +0530

    chenges_path

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,5 +1,5 @@
 import { Inngest } from "inngest";
-import User from "../models/user.js";
+import User from "../models/User.js";
 
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
